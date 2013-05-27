77274e355568c4afc30ca6268763a9e0e7626030
Daniel Guillan <daniel.guillan@gmail.com>
2013-05-27 20:38:59 +0200

    Initial work on jQuery plugin

--- src/js/flyout.js
+++ src/js/flyout.js
@@ -1,36 +1,69 @@
-(function (document, $) {
+;(function($) {
 
-    var $flyout     = $('.flyout'),
-        currentSide;
+    "use strict";
 
-    $('.js-flyout-link').on('click', function(e){
-        var $this = $(this),
-            side = $this.data('flyout-side'),
-            expand = $this.data('flyout-expand');
+    $.flyout = function() {
 
-            if (expand) {
-                flyoutExpand();
-            } else {
-                flyoutClose();
-                side != currentSide ? flyoutOpen(side) : currentSide = undefined;
+        var flyout = {
+            nav: null,
+            currentSide: null,
+            expanded: false,
+
+            open: function(side) {
+                $('body, html').addClass('active-flyout active-flyout--'+side);
+                flyout.nav.filter('.flyout--'+side).addClass('is-active');
+                flyout.currentSide = side;
+            },
+
+            close: function() {
+                if (flyout.expanded) flyout.collapse();
+                $('body, html').removeClass('active-flyout active-flyout--left active-flyout--right');
+                flyout.nav.removeClass('is-active');
+            },
+
+            expand: function() {
+                $('body, html').addClass('active-flyout--expand');
+                flyout.expanded = true;
+            },
+
+            collapse: function() {
+                $('body, html').removeClass('active-flyout--expand');
+                flyout.expanded = false;
+            },
+
+            init: function() {
+
+                flyout.nav = $('.flyout');
+
+                $(document).on('click', '.js-flyout-link', function(e){
+                    var $this = $(this),
+                    side = $this.data('flyout-side'),
+                    expand = $this.data('flyout-expand');
+
+                    if (expand) {
+                        flyout.expanded === true ? flyout.collapse() : flyout.expand();
+                    } else {
+                        flyout.close();
+                        side != flyout.currentSide ? flyout.open(side) : flyout.currentSide = undefined;
+                    }
+                    e.preventDefault(e);
+                });
+
+                return this;
             }
 
-        e.preventDefault(e);
-    });
+        };
 
-    function flyoutOpen(side) {
-        $('body, html').addClass('active-flyout active-flyout--'+side);
-        $flyout.filter('.flyout--'+side).addClass('is-active');
-        currentSide = side;
-    }
+        flyout.init();
 
-    function flyoutClose() {
-        $('body, html').removeClass('active-flyout active-flyout--left active-flyout--right');
-        $flyout.removeClass('is-active');
-    }
+        return {
+            open: flyout.open,
+            close: flyout.close,
+            expand: flyout.expand,
+            collapse: flyout.collapse,
+            init: flyout.init
+        };
+    };
 
-    function flyoutExpand() {
-        $('body, html').toggleClass('active-flyout--expand');
-    }
 
-}(document, jQuery));
+})(jQuery);
