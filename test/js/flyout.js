09689049a4bc1e068a23d9e66f0c97b9d09bffa0
Daniel Guillan <daniel.guillan@gmail.com>
2013-07-02 13:43:23 +0200

    Updated demo with fixes for open/close methods and automatic Flyout object creation

--- test/js/flyout.js
+++ test/js/flyout.js
@@ -1,34 +1,38 @@
-;(function($) {
+;(function($,window) {
 
     "use strict";
 
-    $.flyout = function() {
+    function Flyout() {
 
         var flyout = {
             nav: null,
             currentSide: null,
-            expanded: false,
+            isOpen: false,
+            isExpanded: false,
 
             open: function(side) {
                 $('body, html').addClass('active-flyout active-flyout--'+side);
                 flyout.nav.filter('.flyout--'+side).addClass('is-active');
+                flyout.isOpen = true;
                 flyout.currentSide = side;
             },
 
             close: function() {
-                if (flyout.expanded) flyout.collapse();
+                if (flyout.isExpanded) flyout.collapse();
                 $('body, html').removeClass('active-flyout active-flyout--left active-flyout--right');
                 flyout.nav.removeClass('is-active');
+                flyout.isOpen = false;
+                flyout.currentSide = undefined;
             },
 
             expand: function() {
                 $('body, html').addClass('active-flyout--expand');
-                flyout.expanded = true;
+                flyout.isExpanded = true;
             },
 
             collapse: function() {
                 $('body, html').removeClass('active-flyout--expand');
-                flyout.expanded = false;
+                flyout.isExpanded = false;
             },
 
             init: function() {
@@ -36,15 +40,15 @@
                 flyout.nav = $('.flyout');
 
                 $(document).on('click', '.js-flyout-link', function(e){
+
                     var $this = $(this),
                     side = $this.data('flyout-side'),
                     expand = $this.data('flyout-expand');
 
                     if (expand) {
-                        flyout.expanded === true ? flyout.collapse() : flyout.expand();
+                        flyout.isExpanded === true ? flyout.collapse() : flyout.expand();
                     } else {
-                        flyout.close();
-                        side != flyout.currentSide ? flyout.open(side) : flyout.currentSide = undefined;
+                        flyout.isOpen ? flyout.close() : flyout.open(side);
                     }
                     e.preventDefault(e);
                 });
@@ -60,10 +64,10 @@
             open: flyout.open,
             close: flyout.close,
             expand: flyout.expand,
-            collapse: flyout.collapse,
-            init: flyout.init
+            collapse: flyout.collapse
         };
     };
 
+    window.Flyout = Flyout();
 
-})(jQuery);
+}(jQuery, this));
